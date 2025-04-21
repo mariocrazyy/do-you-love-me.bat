25e69ba17444d40f41e0568da40dcc57cd6e3ac9
mariocrazyy <mariostaykovv721@gmail.com>
2025-04-21 22:52:44 +0300

    Add files via upload

---
+++ Do You Love Me(ENCRYPED).bat
@@ -0,0 +1,36 @@
+ÿþ
+cls
+@echo off
+:1
+color c
+echo Hello, do you love me? (answer)
+set /p input=
+if /i %input%==yes goto love
+if /i %input%==no goto hate
+if /i not %input%==yes,no goto 1
+
+:love
+echo I Love you Too...
+echo This is for you:
+echo. 
+echo       ###       ###
+echo     #######   #######
+echo   ######### #########
+echo  #####################
+echo #######################   i luv u :3
+echo   ###################
+echo     ###############
+echo        ###########
+echo          #######
+echo            ###
+echo              #
+echo               #
+echo               #
+pause
+exit
+
+:hate
+cls
+echo But I Love You.. :(
+start "" "https://www.youtube.com/watch?v=mzB1VGEGcSU&pp=ygUMbHVjaWQgZHJlYW1z"
+pause
